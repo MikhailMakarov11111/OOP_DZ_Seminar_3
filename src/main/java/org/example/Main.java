0893d041a823e1c77cf66358bc4350006690acca
Your Name <mikhailmakarov92@gmail.com>
2024-02-14 12:24:31 +0400

    Добавлен класс EnGame

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        AbstractGame nGame = new RuGame();
+        AbstractGame nGame = new EnGame();
 
         nGame.start(4, 2);
         Scanner scanner = new Scanner(System.in);
